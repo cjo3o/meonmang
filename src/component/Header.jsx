fcc1e9eb352fa1c88b947dc21d0c8516af851abf
cjo3o <cjy55973743@gmail.com>
2025-05-19 18:11:21 +0900

    no message

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -6,20 +6,23 @@ function Header(props) {
     return (
         <>
             <div className={styles.Header}>
-                <div className={styles.Logo}>
-                    <img src={logo} alt="logo" width={"180px"}/>
+                <div className={styles.headerContainer}>
+                    <div className={styles.Logo}>
+                        <img src={logo} alt="logo" width={"180px"}/>
+                    </div>
+                    <div className={styles.nav}>
+                        <ul>
+                            <li>먼망진창 소개</li>
+                            <li>대기 정보</li>
+                            <li>대기오염 알림</li>
+                            <li>고객지원</li>
+                        </ul>
+                    </div>
+                    <div className={styles.kakao}>
+                        <h3>카카오 로그인</h3>
+                    </div>
                 </div>
-                <div className={styles.nav}>
-                    <ul>
-                        <li>먼망진창 소개</li>
-                        <li>대기 정보</li>
-                        <li>대기오염 알림</li>
-                        <li>고객지원</li>
-                    </ul>
-                </div>
-                <div className={styles.kakao}>
-                    <h3>카카오 로그인</h3>
-                </div>
+
             </div>
         </>
     );
