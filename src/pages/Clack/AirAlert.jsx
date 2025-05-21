d3b2aa704d62a4cc552686527683fe671f02a6bb
DESKTOP-29VI7GO\user <kimyoott@gmail.com>
2025-05-21 17:57:35 +0900

    20250521 화면 추가

---
+++ src/pages/Clack/AirAlert.jsx
@@ -0,0 +1,14 @@
+import Header from "../component/Header.jsx";
+import Footer from "../component/Footer.jsx";
+
+import React from "react";
+
+function AirAlert() {
+    return (
+        <>
+            <div>AirAlert</div>
+        </>
+    );
+}
+
+export default AirAlert;
