a6906ce54266a2c6e3e61390f65ba81d0d4c97e9
cjo3o <cjy55973743@gmail.com>
2025-05-21 18:11:07 +0900

    no message

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -1,6 +1,9 @@
-import {Card} from "antd";
+import {Card, Select} from "antd";
 import styles from "/src/css/Map.module.css";
 import {useState} from "react";
+import RealTime from "./RealTime.jsx";
+import DateInfor from "./DateInfor.jsx";
+import RealTimeMap from "./RealTimeMap.jsx";
 
 function Map(props) {
     const [activeTab, setActiveTab] = useState("realTime");
@@ -9,16 +12,21 @@ function Map(props) {
             <Card
                 className={styles.mapCard}
                 title={
-                    <div className={styles.mapTitle}>
-                        <div className={`${styles.realTime} ${activeTab === "realTime" ? styles.active : ""}`}
-                             onClick={() => setActiveTab("realTime")}>
-                            실시간 대기정보
+                    <>
+                        <div className={styles.mapTitle}>
+                            <div className={`${styles.realTime} ${activeTab === "realTime" ? styles.active : ""}`}
+                                 onClick={() => setActiveTab("realTime")}>
+                                실시간 대기정보
+                            </div>
+                            <div className={`${styles.dateInfor} ${activeTab === "dateInfor" ? styles.active : ""}`}
+                                 onClick={() => setActiveTab("dateInfor")}>
+                                오늘/내일 대기정보
+                            </div>
                         </div>
-                        <div className={`${styles.dateInfor} ${activeTab === "dateInfor" ? styles.active : ""}`}
-                             onClick={() => setActiveTab("dateInfor")}>
-                            오늘/내일 대기정보
+                        <div className={styles.subTitle}>
+                            {activeTab === "realTime" ? <RealTime/> : <DateInfor/>}
                         </div>
-                    </div>
+                    </>
                 }
                 variant="borderless"
                 styles={{
@@ -29,7 +37,7 @@ function Map(props) {
                     }
                 }}
             >
-                {activeTab === "realTime" ? "실시간 대기정보" : "오늘/내일 대기정보"}
+                {activeTab === "realTime" ? <RealTimeMap/> : "오늘/내일 대기정보"}
             </Card>
         </>
     );
---
+++ src/component/RealTime.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Select} from "antd";
+import styles from "../css/Map.module.css";
+
+function RealTime(props) {
+    return (
+        <>
+            <Select
+                className={styles.roundSelect}
+                style={{width: "30%", textAlign: "center"}}
+                defaultValue="초미세먼지 (PM-2.5)"
+                options={[
+                    {value: "초미세먼지 (PM-2.5)", label: "초미세먼지 (PM-2.5)"},
+                    {value: "미세먼지 (PM-10)", label: "미세먼지 (PM-10)"},
+                    {value: "오존 (O₃)", label: "오존 (O₃)"},
+                    {value: "이산화질소 (NO₂)", label: "이산화질소 (NO₂)"},
+                    {value: "일산화탄소 (CO)", label: "일산화탄소 (CO)"},
+                    {value: "아황산가스 (SO₂)", label: "아황산가스 (SO₂)"},
+                ]}>
+            </Select>
+            <div className={styles.selectCity}>
+                <p>시/도</p>
+                <Select
+                    className={styles.roundSelect}
+                    style={{width: "100%", textAlign: "center"}}
+                    defaultValue="-전체-"
+                    options={[
+                        {value: "서울특별시", label: "서울특별시"},
+                        {value: "부산광역시", label: "부산광역시"},
+                        {value: "대구광역시", label: "대구광역시"},
+                        {value: "인천광역시", label: "인천광역시"},
+                        {value: "광주광역시", label: "광주광역시"},
+                        {value: "대전광역시", label: "대전광역시"},
+                        {value: "울산광역시", label: "울산광역시"},
+                        {value: "세종특별자치시", label: "세종특별자치시"},
+                        {value: "경기도", label: "경기도"},
+                        {value: "강원도", label: "강원도"},
+                        {value: "충청북도", label: "충청북도"},
+                        {value: "충청남도", label: "충청남도"},
+                        {value: "전라북도", label: "전라북도"},
+                        {value: "전라남도", label: "전라남도"},
+                        {value: "경상북도", label: "경상북도"},
+                        {value: "경상남도", label: "경상남도"},
+                        {value: "제주특별자치도", label: "제주특별자치도"}
+                    ]}>
+                </Select>
+            </div>
+            <div className={styles.time}>
+                2025년 05월 21일 16시
+            </div>
+        </>
+    );
+}
+
+export default RealTime;
