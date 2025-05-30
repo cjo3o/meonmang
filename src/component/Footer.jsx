ea913291bd67a97bc39151f9d0fbbea68abdf500
cjo3o <cjy55973743@gmail.com>
2025-05-30 17:01:47 +0900

    no message

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,17 +4,22 @@ import whiteLogo from '/src/images/WhiteLogo.svg';
 import Top from '/src/images/Top.svg';
 
 function Footer(props) {
+    const moveTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
     return (
         <>
             <div className={styles.Footer}>
                 <div className={styles.footerContainer}>
-                    <img src={whiteLogo} alt="whitelogo" width={"180px"}/>
+                    <div className={styles.logo}>
+                        <img src={whiteLogo} alt="whitelogo" width={"180px"}/>
+                    </div>
                     <div className={styles.footerContent}>
                         <p>(41937) 대구광역시 중구 중앙대로 394 제일빌딩 5층</p>
                         <p>대표번호 053) 572 - 1005</p>
                         <p>개인정보처리방침</p>
                     </div>
-                    <div className={styles.moveTop}>
+                    <div className={styles.moveTop} onClick={moveTop}>
                         <img src={Top} alt="top"/>
                         <p>TOP</p>
                     </div>
