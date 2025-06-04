a6fa0d33642978fe07517c6c77eaf3288e8b39bd
DESKTOP-29VI7GO\user <parkmyounghoi@gmail.com>
2025-06-04 14:18:27 +0900

    no message

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { unstableSetRender } from 'antd';
+
+unstableSetRender((node, container) => {
+  container._reactRoot ||= createRoot(container);
+  const root = container._reactRoot;
+  root.render(node);
+  return async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    root.unmount();
+  };
+});
+
 createRoot(document.getElementById('root')).render(
     <App />
 )
