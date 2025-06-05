d3b9ed20a9a674ca0769c62b77cf74e4e2bb9416
DESKTOP-29VI7GO\user <kimyoott@gmail.com>
2025-06-05 12:50:19 +0900

    no message

--- src/component/AirAlertFilter.jsx
+++ src/component/AirAlertFilter.jsx
@@ -26,11 +26,11 @@ const AirAlertFilter = ({
         </Col>
         <Col>
             <h4>항목</h4>
-            <Radio.Group value={itemCode} onChange={e => setItemCode(e.target.value)}>
-                <Radio value="전체">전체</Radio>
-                <Radio value="PM10">PM10</Radio>
-                <Radio value="PM25">PM2.5</Radio>
-                <Radio value="O3">O3</Radio>
+            <Radio.Group style={{display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"flex-start"}} value={itemCode} onChange={e => setItemCode(e.target.value)}>
+                <Radio style={{whiteSpace:"nowrap"}} value="전체">전체</Radio>
+                <Radio style={{whiteSpace:"nowrap"}} value="PM10">PM10</Radio>
+                <Radio style={{whiteSpace:"nowrap"}} value="PM25">PM2.5</Radio>
+                <Radio style={{whiteSpace:"nowrap"}} value="O3">O3</Radio>
             </Radio.Group>
         </Col>
         <Col>
